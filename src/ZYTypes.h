e9b7819298240dd51159541da68e86d49a0f1e1f
Peng Wu <alexepico@gmail.com>
2014-01-28 15:22:29 +0800

    import ZYTypes.h

---
+++ src/ZYTypes.h
@@ -0,0 +1,34 @@
+/* vim:set et ts=4 sts=4:
+ *
+ * ibus-libzhuyin - New Zhuyin engine based on libzhuyin for IBus
+ *
+ * Copyright (c) 2008-2010 Peng Huang <shawn.p.huang@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef __ZY_TYPES_H_
+#define __ZY_TYPES_H_
+
+#include <glib.h>
+
+namespace ZY {
+
+
+#define MAX_UTF8_LEN 6
+#define MAX_PHRASE_LEN 16
+
+};
+
+#endif
