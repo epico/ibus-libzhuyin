1198e8342b43d318fa20e913aec0a5648c73a562
Peng Wu <alexepico@gmail.com>
2014-02-17 16:15:20 +0800

    import ZYRawEditor.h

---
+++ src/ZYRawEditor.h
@@ -0,0 +1,35 @@
+/* vim:set et ts=4 sts=4:
+ *
+ * ibus-libzhuyin - New Zhuyin engine based on libzhuyin for IBus
+ *
+ * Copyright (c) 2008-2010 Peng Huang <shawn.p.huang@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef __ZY_RAW_EDITOR_
+#define __ZY_RAW_EDITOR_
+
+#include "ZYEditor.h"
+
+namespace ZY {
+
+class RawEditor : public Editor {
+public:
+    RawEditor (ZhuyinProperties &props, Config & config)
+        : Editor (props, config) { }
+};
+
+};
+#endif
