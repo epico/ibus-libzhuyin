680b98018faca431b610b18335a5c05f3aab2f8d
Peng Wu <alexepico@gmail.com>
2014-03-28 10:00:39 +0800

    begin to write symbols functions

---
+++ src/ZYSymbols.h
@@ -0,0 +1,46 @@
+/* vim:set et ts=4 sts=4:
+ *
+ * ibus-libzhuyin - New Zhuyin engine based on libzhuyin for IBus
+ *
+ * Copyright (c) 2014 Peng Wu <alexepico@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __ZY_SYMBOLS_H_
+#define __ZY_SYMBOLS_H_
+
+#include "ZYString.h"
+
+namespace ZY {
+
+bool
+is_easy_symbol(const char key);
+
+int
+find_lookup_key (const String & symbol);
+
+int
+get_choice_list (const char key, gchar ** & choices);
+
+bool
+half_punct_to_full_punct (const char key, String & punct);
+
+bool
+half_english_to_full_english (const char key, String & english);
+
+};
+
+#endif
