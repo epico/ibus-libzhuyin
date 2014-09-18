2a2ffe7d47726f389d331a9f0f694e094c7a4c14
Peng Wu <alexepico@gmail.com>
2014-09-18 16:59:43 +0800

    import ZYSymbolLookup.h

--- src/ZYSymbols.cc
+++ src/ZYSymbols.cc
@@ -27,7 +27,10 @@ namespace ZY {
 /* The following tables are copied from libchewing code. */
 
 static const char G_EASY_SYMBOL_KEY[] = {
+#if 0
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+#endif
+    /* only capital letters are allowed here. */
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 	'U', 'V', 'W', 'X', 'Y', 'Z'
