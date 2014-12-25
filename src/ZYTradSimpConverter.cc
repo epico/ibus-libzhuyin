67e19855ebee95e119d436755a4d5473ee8cb9bd
Peng Wu <alexepico@gmail.com>
2014-12-25 13:11:40 +0800

    use opencc 1.0

--- src/ZYTradSimpConverter.cc
+++ src/ZYTradSimpConverter.cc
@@ -33,7 +33,6 @@
 namespace ZY {
 
 class opencc {
-    static const int BUFFER_SIZE = 64;
 public:
     opencc (void)
     {
@@ -48,30 +47,13 @@ public:
 
     void convert (const gchar *in, String &out)
     {
-        glong n_char;
-        gunichar *in_ucs4 = g_utf8_to_ucs4_fast (in, -1, &n_char);
-
-        ucs4_t *pinbuf = (ucs4_t *)in_ucs4;
-        size_t inbuf_left = n_char;
-        while (inbuf_left != 0) {
-            ucs4_t *poutbuf = (ucs4_t *)m_buffer;
-            size_t outbuf_left = BUFFER_SIZE;
-            size_t retval = opencc_convert(m_cc, &pinbuf, &inbuf_left, &poutbuf, &outbuf_left);
-            if (retval == (size_t) -1) {
-                /* append left chars in pinbuf */
-                g_warning ("opencc_convert return failed");
-                out << (gunichar *) pinbuf;
-                break;
-            }
-            *poutbuf = L'\0';
-            out << m_buffer;
-        }
-        g_free (in_ucs4);
+        char * converted = opencc_convert_utf8 (m_cc, in, -1);
+        out = converted;
+        opencc_convert_utf8_free (converted);
     }
 
 private:
     opencc_t m_cc;
-    gunichar m_buffer[BUFFER_SIZE + 1];
 };
 
 void
