828fa4586307d0886b714aad01ae99512ed9dcdf
Peng Wu <alexepico@gmail.com>
2015-08-05 17:03:03 +0800

    update updateZhuyin method

--- src/ZYZPinyinEditor.cc
+++ src/ZYZPinyinEditor.cc
@@ -88,7 +88,20 @@ PinyinEditor::updateZhuyin (void)
             get_phonetic_section (enhanced_text, start_pos, end_pos, section);
 
             zhuyin_instance_t * instance = m_instances[index];
-            zhuyin_parse_more_full_pinyins (instance, section.c_str ());
+            size_t len = zhuyin_parse_more_full_pinyins
+                (instance, section.c_str ());
+            bool valid = zhuyin_valid_zhuyin_keys (instance);
+            if (!valid) {
+                size_t valid_len = zhuyin_get_parsed_input_length (instance);
+
+                const gchar * str = NULL;
+                zhuyin_get_raw_user_input (instance, &str);
+                section = str;
+
+                /* hack here: usually only one zhuyin key will have problem,
+                   so it is safe to just move the cursor. */
+                m_cursor -= len - valid_len;
+            }
             zhuyin_guess_sentence (instance);
 
             new_text += section;
